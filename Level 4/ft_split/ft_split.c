e08233e3e3a33e3d0e048e8c03553605122c9df6
RACHIDsimo <126823833+rachidIRT@users.noreply.github.com>
2025-04-20 23:31:14 +0100

    Add files via upload

---
+++ Level 4/ft_split/ft_split.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+
+int			ft_isspace(char c)
+{
+	return (c == ' ' || c == '\n' || c == '\t');
+}
+
+int		count_words(char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+		while (*str && ft_isspace(*str))
+			str++;
+		if (*str && !ft_isspace(*str))
+		{
+			count++;
+			while (*str && !ft_isspace(*str))
+				str++;
+		}
+	}
+	return (count);
+}
+
+char	*malloc_word(char *str)
+{
+	char *word;
+	int	i;
+
+	i = 0;
+	while (str[i] && !ft_isspace(str[i]))
+		i++;
+	word = (char *)malloc(sizeof(char) * (i + 1));
+	i = 0;
+	while (str[i] && !ft_isspace(str[i]))
+	{
+		word[i] = str[i];
+		i++;
+	}
+	word[i] = '\0';
+	return (word);
+}
+
+char	**ft_split(char *str)
+{
+	char **arr = (char **)malloc(sizeof(char *) * (count_words(str) + 1));
+	int i = 0;
+	while (*str)
+	{
+		while (*str && ft_isspace(*str))
+			str++;
+		if (*str && !ft_isspace(*str))
+		{
+			arr[i] = malloc_word(str);
+			i++;
+			while (*str && !ft_isspace(*str))
+				str++;
+		}
+	}
+	arr[i] = NULL;
+	return (arr);
+}
+
+// #include <stdio.h>
+
+// int		main(int ac, char **av)
+// {
+// 	char **arr;
+
+// 	char *phrase = "   Hello,   Flavio\t Wuensche!  ";
+// 	arr = ft_split(phrase);
+// 	printf("%s\n", arr[0]);
+// 	printf("%s\n", arr[1]);
+// 	printf("%s\n", arr[2]);
+// 	printf("%s\n", arr[3]);
+// }
